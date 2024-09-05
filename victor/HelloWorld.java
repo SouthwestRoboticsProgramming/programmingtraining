f3c69e467f7dab489376999a7eebbe2885f7c522
The True Big Glungus <164088975+TheSchkloinker@users.noreply.github.com>
2024-09-05 17:09:54 -0500

    Create HelloWorld.java

---
+++ victor/HelloWorld.java
@@ -0,0 +1,19 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+      
+      int spin = 20;
+      String something;
+      String goob = "big glungus";
+      
+      something = "19 dolla fornite card";
+      
+      if (spin >= 20){
+      
+      System.out.println(something);
+      }
+      else System.out.println(goob);
+    }
+}
